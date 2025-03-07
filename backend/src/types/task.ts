8cf4c628a0452984893d8decf2151345a42bd39d
jdu211171 <211171m@jdu.uz>
2025-03-07 10:41:53 +0500

    Backend init

---
+++ backend/src/types/task.ts
@@ -0,0 +1,13 @@
+export interface Task {
+    id?: number;
+    name: string;
+    description?: string;
+    due_date?: string;
+    status: 'Incomplete' | 'Completed';
+    created_at?: string;
+}
+
+export interface TaskFilter {
+    status?: 'Incomplete' | 'Completed';
+    due_date?: string;
+}
